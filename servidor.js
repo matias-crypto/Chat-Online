14e85af08c552bf7b87e848ecf835371dcc44f8f
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-02-11 04:26:04 -0300

    Create servidor.js

---
+++ servidor.js
@@ -0,0 +1,11 @@
+// Ejemplo de servidor Node.js utilizando Express
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
